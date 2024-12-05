baf8429f6ce1986e6a5bc3cdd97ea273d83b01e4
adityasonule01 <absonule@gcoen.ac.in>
2024-12-05 23:00:51 +0530

    loaded env variables first before vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    proxy: {
-      "/api": {
-        target: import.meta.env.FRONTEND_URL,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3001,
+      proxy: {
+        "/api": {
+          target: env.VITE_FRONTEND_URL, // Use loaded environment variable
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
