da8230af70f5cebaed3472b5f7a30159bc956843
adityasonule01 <absonule@gcoen.ac.in>
2024-12-05 22:35:20 +0530

    made an .env file in frontend for dynamic backend api url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002",
+        target: import.meta.env.FRONTEND_URL,
         changeOrigin: true,
       },
     },
