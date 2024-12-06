437b2f7f4edfaeaaaa2636fa5aa90e23425f6868
adityasonule01 <absonule@gcoen.ac.in>
2024-12-06 22:09:53 +0530

    abcd

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
